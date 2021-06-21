c68471efafb347da133a7ab8c037dbfbc669e3f3
Tyler Mire <tyler.mire@revature.net>
2021-06-21 08:25:20 -0700

    Cleaned up code and added gitignore

--- W4HackerRankTylerMire/Program.cs
+++ W4HackerRankTylerMire/Program.cs
@@ -1,26 +1,7 @@
-﻿using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Collections;
-using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-using System.Text;
-using System;
+﻿using System;
 
 class Result
 {
-
-    /*
-     * Complete the 'staircase' function below.
-     *
-     * The function accepts INTEGER n as parameter.
-     */
-
     public static void staircase(int n)
     {
         for(int i = 0; i <= n; i++)
